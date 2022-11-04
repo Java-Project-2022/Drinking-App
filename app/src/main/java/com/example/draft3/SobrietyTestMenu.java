e0e9ff9a5b56cf279bf00832d74da78fc3144356
abigamkelly <amk7r@mtmail.mtsu.edu>
2022-11-04 16:23:27 -0500

    hoochie mama

--- app/src/main/java/com/example/draft3/SobrietyTestMenu.java
+++ app/src/main/java/com/example/draft3/SobrietyTestMenu.java
@@ -1,48 +1,37 @@
 package com.example.draft3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SobrietyTestMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_sobriety_test);
+
         Button button40 = (Button) findViewById(R.id.sentence_checker);
-        button40.setOnClickListener(new View.OnClickListener() {
+        button40.setOnClickListener(view -> TakeSentenceTest());
 
-
-            public void onClick(View view) {
-                TakeImageTest();
-
-                //we need a function for when we do the sentence checker
-            }
-
-        });
         Button button10 = (Button) findViewById(R.id.image_test);
-        button10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TakeImageTest();            }
+        button10.setOnClickListener(view -> TakeImageTest());
 
-        });
         ImageButton Home = (ImageButton) findViewById(R.id.imageButton26);
-        Home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                closeSobrietyTestMenu();
-            }
-        });
+        Home.setOnClickListener(view -> closeSobrietyTestMenu());
     }
     public void TakeImageTest(){
         Intent intent = new Intent(this, ImageTest.class);
         startActivity(intent);
     }
+
+    public void TakeSentenceTest(){
+        Intent intent2 = new Intent(this, WordTest.class);
+        startActivity(intent2);
+    }
+
     public void closeSobrietyTestMenu(){
         Intent intent1 = new Intent(this, MainActivity.class);
         startActivity(intent1);
---
+++ app/src/main/java/com/example/draft3/WordTest.java
@@ -0,0 +1,53 @@
+package com.example.draft3;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class WordTest extends AppCompatActivity {
+
+    String user_word;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spelling_test);
+        char[] characters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4',
+                '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '&', '*'};
+        Random rand = new Random();
+        int character_select;
+        int length = 5 + rand.nextInt(5);
+        String word;
+
+        StringBuilder sb = new StringBuilder();
+
+
+        for (int i = 0; i < length; i++) {
+            character_select = rand.nextInt(42);
+            sb.append(characters[character_select]);
+        }
+        word = sb.toString();
+        TextView textView20 = (TextView) findViewById(R.id.textView20);
+        textView20.setText(word);
+        EditText random_word = (EditText) findViewById(R.id.random_word);
+        Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(v -> {
+            user_word = random_word.getText().toString();
+            TextView textView21 = (TextView) findViewById(R.id.textView21);
+            if (Objects.equals(user_word, word)) {
+                textView21.setText("Correct!");
+            } else {
+                textView21.setText("Incorrect!  Try Again!");
+            }
+        });
+    }
+}
