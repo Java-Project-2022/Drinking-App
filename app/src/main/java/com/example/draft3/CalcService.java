09d4bba7e23c637409eb0408a026aa97552174d6
jasonscotth <shartsell2013@gmail.com>
2022-11-04 17:45:28 -0500

    11/4/22 - Scott H incomplete calculation code introduced 3 new files

--- app/src/main/java/com/example/draft3/CalcService.java
+++ app/src/main/java/com/example/draft3/CalcService.java
@@ -6,28 +6,33 @@ public class CalcService {
     //constant decrement per minute
     final static double decrement = 0.025f;
 
-    public static void bacUpdate() {
-        if(BAC == 0.0f) {
-            //check static array pos and update
-            if(CalcData.myArray[0] != 0.0f) {
-                BAC += CalcData.myArray[0]; }
-            else {
-                //stop service b/c nothing in array and BAC 0
-            }
-        }
-        else {
-            //decrement
-            BAC -= decrement;
-            //check static array pos and update
-            if(CalcData.myArray[0] != 0.0f) {
-                BAC += CalcData.myArray[0];
-            }
-        }
+    static int flag = 1;
 
-        //left shift array contents and initialize pos 29 to 0
-        for(int i=0; i<29; i++) {
-            CalcData.myArray[i] = CalcData.myArray[i+1];
+    public static void bacUpdate() {
+        while(flag == 1) {
+            TimingCode timer = new TimingCode(1);
+            if (BAC == 0.0f) {
+                //check static array pos and update
+                if (CalcData.myArray[0] != 0.0f) {
+                    BAC += CalcData.myArray[0];
+                } else {
+                    //stop service b/c nothing in array and BAC 0
+                    flag = 0;
+                }
+            } else {
+                //decrement
+                BAC -= decrement;
+                //check static array pos and update
+                if (CalcData.myArray[0] != 0.0f) {
+                    BAC += CalcData.myArray[0];
+                }
+            }
+
+            //left shift array contents and initialize pos 29 to 0
+            for (int i = 0; i < 29; i++) {
+                CalcData.myArray[i] = CalcData.myArray[i + 1];
+            }
+            CalcData.myArray[29] = 0.0f;
         }
-        CalcData.myArray[29] = 0.0f;
     }
 }
