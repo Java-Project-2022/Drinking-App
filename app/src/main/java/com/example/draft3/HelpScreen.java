fdd457b99a04a85cf0dba80c96dd2e6dff2ac314
Eraizella <kdh.angel103@gmail.com>
2022-11-18 11:02:05 -0800

    Added Uber button. In order for it to open up Uber you need Uber downloaded on the Emulator but it will take you to the google play store if you dont have it installed

--- app/src/main/java/com/example/draft3/HelpScreen.java
+++ app/src/main/java/com/example/draft3/HelpScreen.java
@@ -1,20 +1,39 @@
 package com.example.draft3;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import com.uber.sdk.android.core.UberSdk;
+import com.uber.sdk.android.rides.RideRequestButton;
+import com.uber.sdk.core.auth.Scope;
+import com.uber.sdk.core.client.SessionConfiguration;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
 
 public class HelpScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help_screen);
-
+        SessionConfiguration sessionConfiguration = new SessionConfiguration.Builder()
+                .setClientId("b_Cx4qgi40QhJh4srjBolK1l8plEbV1F")
+                .setServerToken("LH1tcHn5_z-XsRlinz1umc1cv1ZbxnxW4V4w77Hp")
+                .setRedirectUri("")
+                .setScopes(Arrays.asList(Scope.RIDE_WIDGETS))
+                .setEnvironment(SessionConfiguration.Environment.SANDBOX)
+                .build();
+        UberSdk.initialize(sessionConfiguration);
+        RideRequestButton requestButton = new RideRequestButton(HelpScreen.this);
+        RelativeLayout layout= new RelativeLayout(this);
+        layout.addView(requestButton);
         // Police button
         ImageButton button_police = (ImageButton) findViewById(R.id.policeButton);
         button_police.setOnClickListener(view-> closeHelpScreen());
@@ -24,8 +43,6 @@ public class HelpScreen extends AppCompatActivity {
         button_parking.setOnClickListener(view-> closeHelpScreen());
 
         // Uber Button
-        ImageButton button_uber = (ImageButton) findViewById(R.id.imageButtonUBER_BUTTON);
-        button_uber.setOnClickListener(view-> closeHelpScreen());
 
 
         // Home button to take the user back to the Main Activity
@@ -39,6 +56,8 @@ public class HelpScreen extends AppCompatActivity {
     }
     // UDF to take the user back to Main
     public void closeHelpScreen(){
+        Uri number = Uri.parse("tel:5551234");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
