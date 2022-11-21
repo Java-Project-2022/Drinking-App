4413e2b88dbe6016dcb7997ddc22569ddcbeca22
mattw <matthewwooten50@yahoo.com>
2022-11-21 12:48:40 -0600

    credit screen start

---
+++ app/src/main/java/com/example/MyDrinkingBuddy/Info.java
@@ -0,0 +1,29 @@
+package com.example.MyDrinkingBuddy;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Info  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_screen);
+
+
+        // Home button that takes the user back to the Main Activity
+        ImageButton Home = findViewById(R.id.imageButton31);
+        Home.setOnClickListener(view -> closeInfoMenu());
+    }
+
+    // UDF that takes the user back to the main screen
+    public void closeInfoMenu() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
