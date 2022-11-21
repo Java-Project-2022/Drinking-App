a64080f152497717aed6ff0472e8753f4f58688e
abigamkelly <amk7r@mtmail.mtsu.edu>
2022-11-21 11:47:05 -0600

    Scott's crappy code

--- app/src/main/java/com/example/MyDrinkingBuddy/DrinkMenu.java
+++ app/src/main/java/com/example/MyDrinkingBuddy/DrinkMenu.java
@@ -1,6 +1,10 @@
 package com.example.MyDrinkingBuddy;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,27 +19,28 @@ public class DrinkMenu extends AppCompatActivity {
         setContentView(R.layout.activity_track_drink);
 
         // Home Button
-        ImageButton Home = (ImageButton) findViewById(R.id.imageButton28);
-        Home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                closeDrinkMenu();
-            }
-        });
+        ImageButton Home = findViewById(R.id.imageButton28);
+        Home.setOnClickListener(view -> closeDrinkMenu());
 
         // Champagne Button
-        ImageButton button6 = (ImageButton) findViewById(R.id.imageButton30);
+        ImageButton button6 = findViewById(R.id.imageButton30);
         button6.setOnClickListener(view -> {
             Constants.drink++;
             CalcService.addDrink(SettingsData.weight, SettingsData.gender, 12, 0.05);
-            //if(CalcService.loopActive == 0) {
-            //    CalcService.bacLoop();
-            //}
+            if(CalcService.loopActive == 0) {
+                WorkRequest uploadWorkRequest =
+                        new OneTimeWorkRequest.Builder(BACWorker.class)
+                                .build();
+                Context mycontext = null;
+                WorkManager
+                        .getInstance(mycontext)
+                        .enqueue(uploadWorkRequest);
+            }
             closeDrinkMenu();
         });
 
         // Beer Button
-        ImageButton button5 = (ImageButton) findViewById(R.id.imageButton5);
+        ImageButton button5 = findViewById(R.id.imageButton5);
         button5.setOnClickListener(view -> {
             Constants.drink++;
             CalcService.addDrink(SettingsData.weight, SettingsData.gender, 12, 0.05);
@@ -46,7 +51,7 @@ public class DrinkMenu extends AppCompatActivity {
         });
 
         // Cocktail Button
-        ImageButton button7 = (ImageButton) findViewById(R.id.image_test);
+        ImageButton button7 = findViewById(R.id.image_test);
         button7.setOnClickListener(view -> {
             Constants.drink++;
             CalcService.addDrink(SettingsData.weight, SettingsData.gender, 12, 0.05);
@@ -57,10 +62,8 @@ public class DrinkMenu extends AppCompatActivity {
         });
 
         // Custom Button (currently Wine)
-        ImageButton button4 = (ImageButton) findViewById(R.id.imageButton4);
-        button4.setOnClickListener(view -> {
-            openCustomDrink();
-        });
+        ImageButton button4 = findViewById(R.id.imageButton4);
+        button4.setOnClickListener(view -> openCustomDrink());
     }
     // UDF to close the current menu, takes user back to Main Screen
     public void closeDrinkMenu(){
